3cde763cf906d92e0af0a9076bbbccf09b34d4e4
yanader <mrbarrow@hotmail.co.uk>
2023-09-20 09:00:06 +0000

    commit from pythonanywhere

--- DjangoWeb/settings.py
+++ DjangoWeb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1d8xaas4vmeldf1mkki5yf7=n&k2-=md+2v*18t5+s^tyu@slz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yanader.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/yanader/westaway/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
