987d7752e3e7047116f7ac5e1f0057cdfaa2dce9
Ste <mrbarrow@hotmail.co.uk>
2023-09-28 16:11:10 +0100

    url updated

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("contents.urls")),
-    path('westaway/', include("westaway.urls", namespace="westaway")),
+    path('watchingwest/', include("westaway.urls", namespace="westaway")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
